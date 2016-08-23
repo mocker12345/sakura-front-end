6faa8abe80305f669e0cf7dd86cfc5b85eb2fb85
zhongshan <imzhongshan@126.com>
2016-08-23 09:36:55 +0800

    修复图片无法编译的问题

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -83,9 +83,6 @@ module.exports = {
     },
     // { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
     { test: /\.scss$/, loader: ["style", "css?sourceMap", "sass?sourceMap"]},
-    // {
-    //     test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=50000&name=[path][name].[ext]'
-    // },
     ]
   },
   eslint: {
