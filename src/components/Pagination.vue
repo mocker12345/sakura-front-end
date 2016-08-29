e623123f119dc04cd58620328485a379b16d2a4a
zhongshan <imzhongshan@126.com>
2016-08-29 14:22:30 +0800

    分页功能

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,16 +1,25 @@
 <template lang="html">
     <div class="center row">
         <ul class="pagination">
-            <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-            <li class="active"><a href="#!">1</a></li>
-            <li class="waves-effect"><a href="#!">2</a></li>
-            <li class="waves-effect"><a href="#!">3</a></li>
-            <li class="waves-effect"><a href="#!">4</a></li>
-            <li class="waves-effect"><a href="#!">5</a></li>
-            <li class="waves-effect"><a href="#!">6</a></li>
-            <li class="waves-effect"><a href="#!">7</a></li>
-            <li class="waves-effect"><a href="#!">8</a></li>
-            <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+            <li v-bind:class="{'disabled': isPreDisabled, 'waves-effect': !isPreDisabled}"
+                v-on:click="pre">
+                <a href="#!">
+                    <i class="material-icons">chevron_left</i>
+                </a>
+            </li>
+            <li v-bind:class="{'active': cur==index, 'waves-effect': cur!=index}"
+                v-for="index in pagenums"
+                v-on:click="pageChange(index)">
+                <a >{{index}}</a>
+            </li>
+            <li v-bind:class="{'disabled': isNextDisabled, 'waves-effect': !isNextDisabled}"
+                v-on:click="next">
+                <a href="#!">
+                    <i class="material-icons">
+                        chevron_right
+                    </i>
+                </a>
+            </li>
           </ul>
      </div>
 </template>
@@ -20,12 +29,64 @@ export default {
     name: 'pagination',
     data() {
         return {
+            cur: 1,
+            totalPage: 38,
+            visiblePage: 10,
+            msg: '',
+            isNextDisabled: false,
+            isPreDisabled: true
         };
     },
-    computed: {},
+    computed: {
+        pagenums: function() {
+            var lowPage = 1,
+                highPage = this.totalPage,
+                pageArr =  [];
+            if (this.totalPage > this.visiblePage) {
+                var subVisiblePage = Math.ceil(this.visiblePage/2)
+                if (this.cur > subVisiblePage && this.cur < (this.totalPage - subVisiblePage + 1)) {
+                    lowPage = this.cur - subVisiblePage
+                    highPage = this.cur + subVisiblePage - 1
+                } else if (this.cur <= subVisiblePage) {
+                    lowPage = 1
+                    highPage = this.visiblePage
+                } else {
+                    lowPage = this.totalPage - this.visiblePage + 1
+                    highPage = this.totalPage
+                }
+            }
+            while (lowPage <= highPage) {
+                pageArr.push(lowPage)
+                lowPage++
+            }
+            return pageArr
+        }
+    },
     ready() {},
     attached() {},
-    methods: {},
+    methods: {
+        pageChange: function(page) {
+            if (this.cur != page) {
+                this.cur = page
+                this.isPreDisabled = this.cur==1 ? true : false
+                this.isNextDisabled = this.cur==this.totalPage ? true : false
+            }
+        },
+        pre: function() {
+            if (this.cur > 1) {
+                this.cur--
+            } else {
+                this.cur = 1
+            }
+        },
+        next: function() {
+            if (this.cur < this.totalPage) {
+                this.cur++
+            } else {
+                this.cur = this.totalPage
+            }
+        }
+    },
     components: {}
 };
 </script>
