64832b933b71a71c655e0553041225fc2215c99a
zhongshan <imzhongshan@126.com>
2016-09-18 09:39:22 +0800

    冲突样式调整

--- src/components/HotTopicsItem.vue
+++ src/components/HotTopicsItem.vue
@@ -1,6 +1,6 @@
 <template lang="html">
     <li class="l6 col">
-        <div class="card">
+        <div class="card hot-topics">
             <div class="card-image">
                 <img class="responsive-img" src="../assets/images/office.jpg" height="300" alt="" />
                 <span class="card-title">xxxxxxxxxxxxxxxxxxxxx</span>
@@ -26,21 +26,23 @@ export default {
 </script>
 
 <style lang="scss">
-  .card {
-    height: 130px;
+  .hot-topics {
+    height: 130px!important;
     .card-image {
       height: 100%;
     }
     img {
       height: 100%;
     }
+    .card-title {
+      word-break: break-all;
+      word-wrap: break-word;
+      margin: 0;
+      padding: 0 5px !important;
+      font-size: 15px !important;
+      background: rgba(0,0,0,.4);
+      width: 100%;
+    }
   }
-  .card-title {
-    word-break: break-all;
-    word-wrap: break-word;
-    margin: 0;
-    padding: 0 5px !important;
-    font-size: 15px !important;
-    background: rgba(0,0,0,.4);
-  }
+
 </style>
